a82099cbeba453436573b8ab44adc3046151f230
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2021-04-14 03:13:05 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","ecoculturedb");
+$conex = mysqli_connect("grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","hasqsqxt5mzjk2r2","e3s8o6a4xfwrp5hj","wowvd8p670e8yx6v");
 
 ?>
